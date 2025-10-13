77d0c79c7e175b4b9648f1455a0ea4b43fbd4e3d
deniscrafts <denis.r.d073@gmail.com>
2025-10-13 13:09:23 +0200

    question

--- denis-finanzas/src/components/pages/JoinComminuty.tsx
+++ denis-finanzas/src/components/pages/JoinComminuty.tsx
@@ -4,7 +4,7 @@ const JoinCommunity = () => {
     return (
         <section id="joincommunity" className=" pt-40 w-full items-center justify-center flex flex-col scroll-mt-0 py-40">
             <div className="flex flex-col items-center justify-center text-center mb-20">
-                <h2 className="text-5xl text-[rgba(var(--branding))] font-bold mb-6">Únete a Nuestra Comunidad</h2>
+                <h2 className="text-5xl text-[rgba(var(--branding))] font-bold mb-6">¿Tienes alguna pregunta?</h2>
                 <p className="text-lg text-[rgba(var(--secondaryText))] mb-4">
                     Conéctate con otros usuarios, comparte tus experiencias y aprende más sobre cómo gestionar tus finanzas personales de manera efectiva.
                 </p>
